3c3c4b8ddb1fb150f80dc6c9c9ea260e87f8ea9f
anatol broder <bro@doktorbro.net>
2019-05-06 03:38:47 +1000

    fix the directory

--- script/vnu.sh
+++ script/vnu.sh
@@ -2,4 +2,4 @@
 
 wget -O vnu.zip "https://github.com/validator/validator/releases/download/18.11.5/vnu.jar_18.11.5.zip"
 unzip vnu.zip dist/vnu.jar
-java -jar dist/vnu.jar --skip-non-html --also-check-css --verbose ../source
+java -jar dist/vnu.jar --skip-non-html --also-check-css --verbose ./source
