2e9841d8a9aa0f36aaa7d83e92e73c63fe2cc996
anatol broder <bro@doktorbro.net>
2019-05-06 03:03:34 +1000

    create the html and css validator script

---
+++ script/vnu.sh
@@ -0,0 +1,5 @@
+!#/usr/bin/env sh
+
+wget -O vnu.zip "https://github.com/validator/validator/releases/download/18.11.5/vnu.jar_18.11.5.zip"
+unzip vnu.zip dist/vnu.jar
+java -jar dist/vnu.jar --skip-non-html --also-check-css --verbose ../source
